034939b5e266edf386dbd3335444a004240c5bf1
YuliaTsveleva <tsveleva.yulia@gmail.com>
2021-12-03 16:51:38 +0200

    add config for inputs

--- src/Components/AddContactForm/AddContactForm.js
+++ src/Components/AddContactForm/AddContactForm.js
@@ -1,23 +1,62 @@
 import React, { Component } from 'react';
 import s from './AddContactForm.module.css';
-
+import CONFIG from '../../Data/inputConfig.json';
 class AddContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+  constructor() {
+    super();
+    this.state = {};
+    CONFIG.forEach(field => {
+      this.state[field.name] = '';
+    });
+  }
+
+  //     state = {
+  //     name: '',
+  //     number: '',
+  //   };
 
   handleChange = e => {
     this.setState({
-      [e.target.name]: [e.target.value],
+      [e.target.name]: e.target.value,
     });
   };
 
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
-    const { name, number } = this.state;
     return (
       <form className={s.Form}>
-        <input
+        {CONFIG.map(field => (
+          <div key={field.name}>
+            <label>{field.label}</label>
+            <input
+              value={this.state[field.name]}
+              onChange={this.handleChange}
+              className={s.Input}
+              type={field.type}
+              name={field.name}
+              pattern={field.pattern}
+              title={field.title}
+              required
+            />
+          </div>
+        ))}
+        <button type="submit" onClick={this.handleSubmit}>
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+export default AddContactForm;
+
+{
+  /* <label>Name</label> */
+  /* <input
           value={name}
           onChange={this.handleChange}
           className={s.Input}
@@ -27,6 +66,7 @@ class AddContactForm extends Component {
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
+        <label>Number</label>
         <input
           value={number}
           onChange={this.handleChange}
@@ -36,10 +76,5 @@ class AddContactForm extends Component {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
-        />
-      </form>
-    );
-  }
+        /> */
 }
-
-export default AddContactForm;
